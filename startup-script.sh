67704597bafde96d848d005e17a5fd18fb9012eb
user10 <user10@WSICAI.IN>
2025-12-06 09:51:21 +0530

    FastAPI with Docker

--- startup-script.sh
+++ startup-script.sh
@@ -57,7 +57,7 @@ APP_DIR="/opt/sql-backend"
 # NOTE: You need to decide how code gets here. 
 # Option A: Git Clone (Uncomment and fill details)
 echo "Cloning repository..."
-sudo git clone  https://github.com/winssoftdev-spec/RAG_GCLOUD.git$APP_DIR
+sudo git clone https://github.com/winssoftdev-spec/RAG_GCLOUD.git $APP_DIR
 
 # Option B: Assume files are uploaded via GCE Metadata or SCP.
 # For this script, we'll assume the user might manually place files or use another method. 
